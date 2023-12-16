startergo/virglrenderer

commit ad8ca0aacd28c1e791c8097a7fbe15602fb17d53

vrend: Always fallback to GL if a GBM transfer fails
This completes the fix in 6a285a1f0e
    vrend: When the GBM upload code path fails, fall back to use OpenGL

Signed-off-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/1315>

src/vrend_renderer.c
@@ -9932,11 +9932,14 @@ int vrend_renderer_copy_transfer3d(struct vrend_context *ctx,
       }
 
       if (use_gbm) {
-         return virgl_gbm_transfer(dst_res->gbm_bo,
-                                   VIRGL_TRANSFER_TO_HOST,
-                                   src_res->iov,
-                                   src_res->num_iovs,
-                                   info);
+         bool success = virgl_gbm_transfer(dst_res->gbm_bo,
+                                           VIRGL_TRANSFER_TO_HOST,
+                                           src_res->iov,
+                                           src_res->num_iovs,
+                                           info) == 0;
+         if (success)
+            return success;
+         virgl_warn("GBM copy transfer failed, try GL\n");
       }
    }
 #endif
@@ -10009,11 +10012,14 @@ int vrend_renderer_copy_transfer3d_from_host(struct vrend_context *ctx,
       }
 
       if (use_gbm) {
-         return virgl_gbm_transfer(src_res->gbm_bo,
-                                   VIRGL_TRANSFER_FROM_HOST,
-                                   dst_res->iov,
-                                   dst_res->num_iovs,
-                                   info);
+         bool success = virgl_gbm_transfer(dst_res->gbm_bo,
+                                           VIRGL_TRANSFER_FROM_HOST,
+                                           src_res->iov,
+                                           src_res->num_iovs,
+                                           info) == 0;
+         if (success)
+            return success;
+         virgl_warn("GBM read failed, try GL\n");
       }
    }
 #endif

commit 4a6001c9cc44e4c453c820042dbb51d0c14f4a0a

vrend: fix gbm transfer params and return value
Fixes regression in the calling and return semantics for two calls to
virgl_gbm_transfer.

Closes: #517
Fixes: ad8ca0aacd ("vrend: Always fallback to GL if a GBM transfer fails")
Signed-off-by: Ryan Neph <ryanneph@google.com>
Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/1318>

src/vrend_renderer.c
@@ -9942,7 +9942,7 @@ int vrend_renderer_copy_transfer3d(struct vrend_context *ctx,
                                            src_res->num_iovs,
                                            info) == 0;
          if (success)
-            return success;
+            return 0;
          virgl_warn("GBM copy transfer failed, try GL\n");
       }
    }
@@ -10016,13 +10016,13 @@ int vrend_renderer_copy_transfer3d_from_host(struct vrend_context *ctx,
       }
 
       if (use_gbm) {
-         bool success = virgl_gbm_transfer(dst_res->gbm_bo,
+         bool success = virgl_gbm_transfer(src_res->gbm_bo,
                                            VIRGL_TRANSFER_FROM_HOST,
-                                           src_res->iov,
-                                           src_res->num_iovs,
+                                           dst_res->iov,
+                                           dst_res->num_iovs,
                                            info) == 0;
          if (success)
-            return success;
+            return 0;
          virgl_warn("GBM read failed, try GL\n");
       }
    }