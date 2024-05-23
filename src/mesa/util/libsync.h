4c7caee7e90d1a4a8385bfff0029bf8676e80f3f
Corentin Noël <corentin.noel@collabora.com>
2024-05-23 14:48:26 +0200

    libsync: Synchronize the file with Mesa version
    Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/1371>

--- src/mesa/util/libsync.h
+++ src/mesa/util/libsync.h
@@ -30,13 +30,15 @@
 
 #include <assert.h>
 #include <errno.h>
+#include <poll.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <string.h>
 #include <sys/ioctl.h>
-#include <sys/poll.h>
 #include <unistd.h>
 #include <time.h>
+
+#include "util/detect_os.h"
 
 #if defined(__GNUC__)
 #pragma GCC diagnostic push
@@ -48,7 +50,7 @@
 extern "C" {
 #endif
 
-#ifdef ANDROID
+#if DETECT_OS_ANDROID
 /* On Android, rely on the system's libsync instead of rolling our own
  * sync_wait() and sync_merge().  This gives us compatibility with pre-4.7
  * Android kernels.
@@ -163,7 +165,7 @@ sync_valid_fd(int fd)
 	return ioctl(fd, SYNC_IOC_FILE_INFO, &info) >= 0;
 }
 
-#endif /* !ANDROID */
+#endif /* DETECT_OS_ANDROID */
 
 /* accumulate fd2 into fd1.  If *fd1 is not a valid fd then dup fd2,
  * otherwise sync_merge() and close the old *fd1.  This can be used
