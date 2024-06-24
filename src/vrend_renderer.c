startergo/virglrenderer

commit 2fcee2ef2bfc91692508c9dba1ffef1dd54e7f81

vrend: conditionally build using gbm_bo_get_map_info()
Use the more accurate gbm_bo_get_map_info() API recently provided by
minigbm when it is available.

This fixes issues with Intel devices that don't have a shared LLC
between CPU and GPU cores to enforce coherency of cached accesses.

It also ensures forward-compatability with other drivers that don't
match the heuristic here.

Signed-off-by: Ryan Neph <ryanneph@google.com>
Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/1380>

src/vrend_renderer.c
@@ -8608,11 +8608,7 @@ static void vrend_resource_gbm_init(struct vrend_resource *gr, uint32_t format)
    gr->gbm_bo = bo;
    gr->storage_bits |= VREND_STORAGE_GBM_BUFFER;
 
-   /* This is true so far, but maybe gbm_bo_get_caching_type is needed in the future. */
-   if (!strcmp(gbm_device_get_backend_name(gbm->device), "i915"))
-      gr->map_info = VIRGL_RENDERER_MAP_CACHE_CACHED;
-   else
-      gr->map_info = VIRGL_RENDERER_MAP_CACHE_WC;
+   gr->map_info = virgl_gbm_get_map_info(bo);
 
    if (!virgl_gbm_gpu_import_required(gr->base.bind))
       return;

src/vrend_winsys_gbm.c
@@ -518,7 +518,45 @@ err_close:
    query->out_num_fds = 0;
    return ret;
 }
+
+uint32_t virgl_gbm_get_map_info(struct gbm_bo *bo) {
+   uint32_t map_info;
+#if defined(MINIGBM) && defined(MINIGBM_HAS_GBM_BO_GET_MAP_INFO)
+      enum gbm_bo_map_cache_mode cache_mode = gbm_bo_get_map_info(bo);
+      switch(cache_mode) {
+      /* case GBM_BO_MAP_CACHE_NONE: */
+      /*    map_info = VIRGL_RENDERER_MAP_CACHE_NONE; */
+      /*    break; */
+      case GBM_BO_MAP_CACHE_CACHED:
+         map_info = VIRGL_RENDERER_MAP_CACHE_CACHED;
+         break;
+      /* case GBM_BO_MAP_CACHE_UNCACHED: */
+      /*    map_info = VIRGL_RENDERER_MAP_CACHE_UNCACHED; */
+      /*    break; */
+      case GBM_BO_MAP_CACHE_WC:
+      default:
+         map_info = VIRGL_RENDERER_MAP_CACHE_WC;
+         break;
+      }
+#else
+   /* That GPU allocations on all intel devices are CACHED is famously untrue
+    * on Intel devices that don't have a shared last-level-cache architecture to
+    * ensure memory coherency between CPU/GPU cores (e.g. GLK). Thus, mistakenly
+    * reporting CACHED mapping type would cause stale cached CPU access to GPU
+    * writes that are resident only in system memory.
+    *
+    * Still this heuristic is retained as a fallback for the missing minigbm API
+    */
+   if (!strcmp(gbm_device_get_backend_name(gbm->device), "i915"))
+      map_info = VIRGL_RENDERER_MAP_CACHE_CACHED;
+   else
+      map_info = VIRGL_RENDERER_MAP_CACHE_WC;
+
+   (void)bo;
 #endif
+   return map_info;
+}
+#endif // #ifdef ENABLE_MINIGBM_ALLOCATION
 
 int virgl_gbm_export_fd(struct gbm_device *gbm, uint32_t handle, int32_t *out_fd)
 {

src/vrend_winsys_gbm.h
@@ -75,6 +75,8 @@ struct gbm_device_info {
 
 #define GBM_DETECT_FLAG_CONNECTED (1u << 0) /* Check if any connectors are connected. SLOW! */
 
+uint32_t virgl_gbm_get_map_info(struct gbm_bo *bo);
+
 #ifdef MINIGBM
 int gbm_detect_device_info(unsigned int detect_flags, int fd, struct gbm_device_info *info);
 int gbm_detect_device_info_path(unsigned int detect_flags, const char *dev_node,

commit cd0b9ebdf9ad7c4be94fc0a74b69c03a8c50fc55

vrend: add a missed gbm map info query
Forgot to replace this instance of the legacy assumption with the new
minigbm map info query API.

Fixes: 2fcee2ef2b ("vrend: conditionally build using gbm_bo_get_map_info()")
Signed-off-by: Ryan Neph <ryanneph@google.com>
Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/1386>

src/vrend_renderer.c
@@ -8324,11 +8324,7 @@ static void vrend_create_buffer(struct vrend_resource *gr, uint32_t width, uint3
          gr->gbm_bo = bo;
          gr->memobj = memobj;
          gr->storage_bits |= VREND_STORAGE_GBM_BUFFER | VREND_STORAGE_GL_MEMOBJ;
-
-         if (!strcmp(gbm_device_get_backend_name(gbm->device), "i915"))
-            gr->map_info = VIRGL_RENDERER_MAP_CACHE_CACHED;
-         else
-            gr->map_info = VIRGL_RENDERER_MAP_CACHE_WC;
+         gr->map_info = virgl_gbm_get_map_info(bo);
       }
 #endif
       else {