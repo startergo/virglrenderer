startergo/virglrenderer

commit e933cd5a38eddb5cb16ea99f30cb33a7090f850f

vkr: handle ring fatal error more robustly
so that to not block vkr main thread synchronous cmd indefinitely.

Signed-off-by: Yiwei Zhang <zzyiwei@chromium.org>
Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/1378>

src/venus/vkr_context.c
@@ -406,11 +406,35 @@ vkr_seqno_ge(uint32_t a, uint32_t b)
 void
 vkr_context_on_ring_seqno_update(struct vkr_context *ctx,
                                  uint64_t ring_id,
-                                 uint64_t ring_seqno)
+                                 uint32_t ring_seqno)
 {
    mtx_lock(&ctx->wait_ring.mutex);
    if (ctx->wait_ring.id == ring_id && vkr_seqno_ge(ring_seqno, ctx->wait_ring.seqno))
       cnd_signal(&ctx->wait_ring.cond);
+   mtx_unlock(&ctx->wait_ring.mutex);
+}
+
+bool
+vkr_context_on_ring_empty(struct vkr_context *ctx, uint64_t ring_id, uint32_t ring_seqno)
+{
+   /* Error out if the last ring cmd is unable to signal the virtqueue that is currently
+    * waiting for this ring. This happens when the driver emits invalid asynchronous ring
+    * wait cmds.
+    */
+   mtx_lock(&ctx->wait_ring.mutex);
+   bool ok =
+      ctx->wait_ring.id != ring_id || vkr_seqno_ge(ring_seqno, ctx->wait_ring.seqno);
+   mtx_unlock(&ctx->wait_ring.mutex);
+   return ok;
+}
+
+void
+vkr_context_on_ring_fatal(struct vkr_context *ctx)
+{
+   vkr_context_set_fatal(ctx);
+
+   mtx_lock(&ctx->wait_ring.mutex);
+   cnd_signal(&ctx->wait_ring.cond);
    mtx_unlock(&ctx->wait_ring.mutex);
 }
 
@@ -426,7 +450,8 @@ vkr_context_wait_ring_seqno(struct vkr_context *ctx,
    mtx_lock(&ctx->wait_ring.mutex);
    ctx->wait_ring.id = ring->id;
    ctx->wait_ring.seqno = ring_seqno;
-   while (ok && !vkr_seqno_ge(vkr_ring_load_head(ring), ring_seqno)) {
+   while (!vkr_context_get_fatal(ctx) && ok &&
+          !vkr_seqno_ge(vkr_ring_load_head(ring), ring_seqno)) {
       ok = cnd_wait(&ctx->wait_ring.cond, &ctx->wait_ring.mutex) == thrd_success;
    }
    ctx->wait_ring.id = 0;

src/venus/vkr_context.h
@@ -246,7 +246,13 @@ vkr_context_get_object(struct vkr_context *ctx, vkr_object_id obj_id)
 void
 vkr_context_on_ring_seqno_update(struct vkr_context *ctx,
                                  uint64_t ring_id,
-                                 uint64_t ring_seqno);
+                                 uint32_t ring_seqno);
+
+bool
+vkr_context_on_ring_empty(struct vkr_context *ctx, uint64_t ring_id, uint32_t ring_seqno);
+
+void
+vkr_context_on_ring_fatal(struct vkr_context *ctx);
 
 bool
 vkr_context_wait_ring_seqno(struct vkr_context *ctx,

src/venus/vkr_ring.c
@@ -297,12 +297,19 @@ vkr_ring_thread(void *arg)
          last_submit = vkr_ring_now();
          relax_iter = 0;
       } else {
+         if (!vkr_context_on_ring_empty(ctx, ring->id, vkr_ring_load_head(ring))) {
+            ret = -EINVAL;
+            break;
+         }
+
          vkr_ring_relax(&relax_iter);
       }
    }
 
-   if (ret < 0)
+   if (ret < 0) {
       vkr_ring_set_status_bits(ring, VK_RING_STATUS_FATAL_BIT_MESA);
+      vkr_context_on_ring_fatal(ctx);
+   }
 
    return ret;
 }

src/venus/vkr_transport.c
@@ -387,6 +387,9 @@ vkr_dispatch_vkWaitRingSeqnoMESA(struct vn_dispatch_context *dispatch,
       return;
    }
 
+   /* ensure to wake ring up to properly error out invalid ring wait */
+   vkr_ring_notify(ring);
+
    if (!vkr_context_wait_ring_seqno(ctx, ring, args->seqno))
       vkr_context_set_fatal(ctx);
 }

commit 2b36f1848fd71612d8ba7421399c439e00f2893d

vkr: fix a prior ring wait seqno validation
Should retrieve the wait seqno first and then compare with latest ring
tail.

Fixes: e933cd5a ("vkr: handle ring fatal error more robustly")
Signed-off-by: Yiwei Zhang <zzyiwei@chromium.org>
Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/1390>

src/venus/vkr_common.h
@@ -317,4 +317,11 @@ vkr_region_make_relative(const struct vkr_region *region)
    };
 }
 
+static inline bool
+vkr_seqno_ge(uint32_t a, uint32_t b)
+{
+   /* a >= b, but deal with wrapping as well */
+   return (a - b) <= INT32_MAX;
+}
+
 #endif /* VKR_COMMON_H */

src/venus/vkr_context.c
@@ -396,13 +396,6 @@ vkr_context_destroy_resource(struct vkr_context *ctx, uint32_t res_id)
    vkr_context_remove_resource(ctx, res_id);
 }
 
-static inline bool
-vkr_seqno_ge(uint32_t a, uint32_t b)
-{
-   /* a >= b, but deal with wrapping as well */
-   return (a - b) <= INT32_MAX;
-}
-
 void
 vkr_context_on_ring_seqno_update(struct vkr_context *ctx,
                                  uint64_t ring_id,
@@ -415,21 +408,18 @@ vkr_context_on_ring_seqno_update(struct vkr_context *ctx,
 }
 
 bool
-vkr_context_on_ring_empty(struct vkr_context *ctx, uint64_t ring_id, uint32_t ring_seqno)
+vkr_context_get_wait_ring_seqno(struct vkr_context *ctx,
+                                uint64_t ring_id,
+                                uint32_t *out_seqno)
 {
-   /* Error out if the last ring cmd is unable to signal the virtqueue that is currently
-    * waiting for this ring. This happens when the driver emits invalid asynchronous ring
-    * wait cmds.
-    */
+   bool wait_ring = false;
    mtx_lock(&ctx->wait_ring.mutex);
-   const bool ok =
-      ctx->wait_ring.id != ring_id || vkr_seqno_ge(ring_seqno, ctx->wait_ring.seqno);
-   if (unlikely(!ok)) {
-      vkr_log("%s: wait for empty ring at seqno(%u) to reach seqno(%u)", __func__,
-              ring_seqno, ctx->wait_ring.seqno);
+   if (ctx->wait_ring.id == ring_id) {
+      wait_ring = true;
+      *out_seqno = ctx->wait_ring.seqno;
    }
    mtx_unlock(&ctx->wait_ring.mutex);
-   return ok;
+   return wait_ring;
 }
 
 void

src/venus/vkr_context.h
@@ -249,7 +249,9 @@ vkr_context_on_ring_seqno_update(struct vkr_context *ctx,
                                  uint32_t ring_seqno);
 
 bool
-vkr_context_on_ring_empty(struct vkr_context *ctx, uint64_t ring_id, uint32_t ring_seqno);
+vkr_context_get_wait_ring_seqno(struct vkr_context *ctx,
+                                uint64_t ring_id,
+                                uint32_t *out_seqno);
 
 void
 vkr_context_on_ring_fatal(struct vkr_context *ctx);

src/venus/vkr_ring.c
@@ -299,9 +299,20 @@ vkr_ring_thread(void *arg)
          last_submit = vkr_ring_now();
          relax_iter = 0;
       } else {
-         if (!vkr_context_on_ring_empty(ctx, ring->id, vkr_ring_load_head(ring))) {
-            ret = -EINVAL;
-            break;
+         /* Get the active wait_ring seqno first to ensure ordering. */
+         uint32_t wait_ring_seqno = 0;
+         if (vkr_context_get_wait_ring_seqno(ctx, ring->id, &wait_ring_seqno)) {
+            /* Error out if the latest ring cmd is unable to signal the virtqueue that is
+             * currently waiting for this ring. This happens when the driver emits invalid
+             * asynchronous ring wait cmds.
+             */
+            const uint32_t ring_tail = vkr_ring_load_tail(ring);
+            if (unlikely(!vkr_seqno_ge(ring_tail, wait_ring_seqno))) {
+               vkr_log("%s: ring seqno(%u) unable to reach wait seqno(%u)", __func__,
+                       ring_tail, wait_ring_seqno);
+               ret = -EINVAL;
+               break;
+            }
          }
 
          vkr_ring_relax(&relax_iter);