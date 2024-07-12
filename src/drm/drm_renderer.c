startergo/virglrenderer

commit 3eb95d3f80f9244d8e647ac75f8649f00ccbfbb5

drm/msm: Use explicitly sized types
This does not fix any actual bugs on any supported platform.  Instead,
it avoids reviewers having to consider specifics of the target platforms
when reviewing the code.

Signed-off-by: Demi Marie Obenour <demi@invisiblethingslab.com>
Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/1382>

src/drm/msm/msm_renderer.c
@@ -617,13 +617,14 @@ msm_renderer_get_blob(struct virgl_context *vctx, uint32_t res_id, uint64_t blob
 }
 
 static void *
-msm_context_rsp(struct msm_context *mctx, const struct vdrm_ccmd_req *hdr, unsigned len)
+msm_context_rsp(struct msm_context *mctx, const struct vdrm_ccmd_req *hdr, size_t len)
 {
-   unsigned rsp_mem_sz = mctx->rsp_mem_sz;
-   unsigned off = hdr->rsp_off;
+   uint32_t rsp_mem_sz = mctx->rsp_mem_sz;
+   uint32_t off = hdr->rsp_off;
 
    if ((off > rsp_mem_sz) || (len > rsp_mem_sz - off)) {
-      drm_log("invalid shm offset: off=%u, len=%u (shmem_size=%u)", off, len, rsp_mem_sz);
+      drm_log("invalid shm offset: off=%" PRIu32
+              ", len=%zu (shmem_size=%" PRIu32 ")", off, len, rsp_mem_sz);
       return NULL;
    }
 
@@ -652,10 +653,10 @@ msm_ccmd_ioctl_simple(struct msm_context *mctx, const struct vdrm_ccmd_req *hdr)
 {
    const struct msm_ccmd_ioctl_simple_req *req = to_msm_ccmd_ioctl_simple_req(hdr);
    unsigned payload_len = _IOC_SIZE(req->cmd);
-   unsigned req_len = size_add(sizeof(*req), payload_len);
+   size_t req_len = size_add(sizeof(*req), payload_len);
 
    if (hdr->len != req_len) {
-      drm_log("%u != %u", hdr->len, req_len);
+      drm_log("%zu != %u", hdr->len, req_len);
       return -EINVAL;
    }
 
@@ -678,7 +679,7 @@ msm_ccmd_ioctl_simple(struct msm_context *mctx, const struct vdrm_ccmd_req *hdr)
    }
 
    struct msm_ccmd_ioctl_simple_rsp *rsp;
-   unsigned rsp_len = sizeof(*rsp);
+   size_t rsp_len = sizeof(*rsp);
 
    if (req->cmd & IOC_OUT)
       rsp_len = size_add(rsp_len, payload_len);

commit cd031235f98549808a60169ba42583e476a4aa4a

Type-check drm_log arguments
drm_log takes a format string, but compilers were not aware of it, so
they could not issue warnings when it did not match the arguments.

Fixes: 3eb95d3f80f9 ("drm/msm: Use explicitly sized types")
Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/1393>

meson.build
@@ -64,6 +64,7 @@ warnings = [
    '-Werror=switch',
    '-Wno-overlength-strings',
    '-Wno-missing-field-initializers',
+   '-Werror=format',
 ]
 
 add_project_arguments(cc.get_supported_arguments(warnings), language : 'c')

src/drm/drm_renderer.c
@@ -119,7 +119,7 @@ size_t
 drm_renderer_capset(void *_c)
 {
    struct virgl_renderer_capset_drm *c = _c;
-   drm_log("c=%p", c);
+   drm_log("c=%p", _c);
 
    if (c)
       *c = capset;

src/drm/drm_util.h
@@ -21,7 +21,7 @@
 
 #include "linux/overflow.h"
 
-void _drm_log(const char *fmt, ...);
+void _drm_log(const char *fmt, ...) __attribute__((format(printf, 1, 2)));
 #define drm_log(fmt, ...) _drm_log("%s:%d: " fmt, __func__, __LINE__, ##__VA_ARGS__)
 
 #pragma GCC diagnostic pop

src/drm/msm/msm_renderer.c
@@ -656,7 +656,7 @@ msm_ccmd_ioctl_simple(struct msm_context *mctx, const struct vdrm_ccmd_req *hdr)
    size_t req_len = size_add(sizeof(*req), payload_len);
 
    if (hdr->len != req_len) {
-      drm_log("%zu != %u", hdr->len, req_len);
+      drm_log("%u != %zu", hdr->len, req_len);
       return -EINVAL;
    }
 