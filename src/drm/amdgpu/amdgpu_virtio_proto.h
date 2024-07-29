startergo/virglrenderer

commit 8636d7257e8045c0389ee8c677ca4767a420a0e5

drm/amdgpu: Use flags instead of integers used as booleans
This replaces some integers that could only take the values 0 or 1 with
flags fields.  It also repurposes the high 32 bits of
struct amdgpu_ccmd_bo_va_op_req::flags for protocol use, as only the low
32 bits were actually passed to the kernel.

This is a highly backwards-incompatible change and so has been moved to
its own commit.

Signed-off-by: Demi Marie Obenour <demi@invisiblethingslab.com>
Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/1383>

src/drm/amdgpu/amdgpu_virtio_proto.h
@@ -107,14 +107,16 @@ struct amdgpu_ccmd_bo_va_op_req {
    struct vdrm_ccmd_req hdr;
    uint64_t va;
    uint64_t vm_map_size;
-   uint64_t flags;
+   uint32_t flags; /* Passed directly to kernel */
+   uint32_t flags2; /* AMDGPU_CCMD_BO_VA_OP_* */
    uint64_t offset;
    uint32_t res_id;
    uint32_t op;
-   uint64_t is_sparse_bo;
 };
 DEFINE_CAST(vdrm_ccmd_req, amdgpu_ccmd_bo_va_op_req)
 AMDGPU_STATIC_ASSERT_SIZE(amdgpu_ccmd_bo_va_op_req)
+/* Specifies that this is a sparse BO. */
+#define AMDGPU_CCMD_BO_VA_OP_SPARSE_BO (UINT64_C(1) << 32)
 
 /*
  * AMDGPU_CCMD_CS_SUBMIT
@@ -197,10 +199,12 @@ struct amdgpu_ccmd_create_ctx_req {
       int32_t priority; /* create */
       uint32_t id;      /* destroy */
    };
-   uint32_t create;
+   uint32_t flags; /* AMDGPU_CCMD_CREATE_CTX_* */
 };
 DEFINE_CAST(vdrm_ccmd_req, amdgpu_ccmd_create_ctx_req)
 AMDGPU_STATIC_ASSERT_SIZE(amdgpu_ccmd_create_ctx_req)
+/* Destroy a context instead of creating one */
+#define AMDGPU_CCMD_CREATE_CTX_DESTROY (1 << 0)
 
 struct amdgpu_ccmd_create_ctx_rsp {
    struct amdgpu_ccmd_rsp hdr;
@@ -214,11 +218,12 @@ AMDGPU_STATIC_ASSERT_SIZE(amdgpu_ccmd_create_ctx_rsp)
  */
 struct amdgpu_ccmd_reserve_vmid_req {
    struct vdrm_ccmd_req hdr;
-   uint32_t enable;
-   uint32_t pad; /* must be zero */
+   uint64_t flags; /* AMDGPU_CCMD_RESERVE_VMID_* */
 };
 DEFINE_CAST(vdrm_ccmd_req, amdgpu_ccmd_reserve_vmid_req)
 AMDGPU_STATIC_ASSERT_SIZE(amdgpu_ccmd_reserve_vmid_req)
+/* Unreserve a VMID instead of reserving one */
+#define AMDGPU_CCMD_RESERVE_VMID_UNRESERVE (1 << 0)
 
 /*
  * AMDGPU_CCMD_SET_PSTATE

src/drm/amdgpu/amdgpu_renderer.c
@@ -744,7 +744,13 @@ amdgpu_ccmd_bo_va_op(struct amdgpu_context *ctx, const struct vdrm_ccmd_req *hdr
       return -ENOMEM;
    }
 
-   if (req->is_sparse_bo) {
+   if (req->flags2 & ~AMDGPU_CCMD_BO_VA_OP_SPARSE_BO) {
+      print(0, "Forbidden flags 0x%" PRIx32 " set in flags", req->flags);
+      rsp->ret = -EINVAL;
+      return -1;
+   }
+
+   if (req->flags2 & AMDGPU_CCMD_BO_VA_OP_SPARSE_BO) {
       rsp->ret = amdgpu_bo_va_op_raw(
          ctx->dev, NULL, req->offset, req->vm_map_size, req->va,
          req->flags, req->op);
@@ -889,7 +895,13 @@ amdgpu_ccmd_create_ctx(struct amdgpu_context *ctx, const struct vdrm_ccmd_req *h
       return -ENOMEM;
    }
 
-   if (req->create) {
+   if (req->flags & ~AMDGPU_CCMD_CREATE_CTX_DESTROY) {
+      print(0, "Invalid flags 0x%" PRIu32, req->flags);
+      rsp->hdr.ret = -EINVAL;
+      return -1;
+   }
+
+   if (!(req->flags & AMDGPU_CCMD_CREATE_CTX_DESTROY)) {
       amdgpu_context_handle ctx_handle;
 
       int r = amdgpu_cs_ctx_create2(ctx->dev, req->priority, &ctx_handle);
@@ -1207,14 +1219,14 @@ static int amdgpu_ccmd_reserve_vmid(struct amdgpu_context *ctx, const struct vdr
       return -ENOMEM;
    }
 
-   if (req->pad != 0) {
-      print(0, "Padding not zeroed");
+   if (req->flags & ~AMDGPU_CCMD_RESERVE_VMID_UNRESERVE) {
+      print(0, "Invalid flags 0x%" PRIu64, req->flags);
       rsp->ret = -EINVAL;
       return -1;
    }
 
-   rsp->ret = req->enable ?
-         amdgpu_vm_reserve_vmid(ctx->dev, 0) : amdgpu_vm_unreserve_vmid(ctx->dev, 0);
+   rsp->ret = (req->flags & AMDGPU_CCMD_RESERVE_VMID_UNRESERVE) ?
+         amdgpu_vm_unreserve_vmid(ctx->dev, 0) : amdgpu_vm_reserve_vmid(ctx->dev, 0);
    return 0;
 }
 

commit e3e2dccc0b6441b559bc64dd97bb39514c11a871

drm: amdgpu: Fix sparse-bo flag
The "flags" variable is now 32bit, while the sparse-bo flag was left
to be 64bit value. Change sparse-bo flag to be within 32bit range.
Fixes Vulkan games not working.

Fixes: 8636d7257e80 ("drm/amdgpu: Use flags instead of integers used as booleans ")
Signed-off-by: Dmitry Osipenko <dmitry.osipenko@collabora.com>
Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/1411>

src/drm/amdgpu/amdgpu_virtio_proto.h
@@ -116,7 +116,7 @@ struct amdgpu_ccmd_bo_va_op_req {
 DEFINE_CAST(vdrm_ccmd_req, amdgpu_ccmd_bo_va_op_req)
 AMDGPU_STATIC_ASSERT_SIZE(amdgpu_ccmd_bo_va_op_req)
 /* Specifies that this is a sparse BO. */
-#define AMDGPU_CCMD_BO_VA_OP_SPARSE_BO (UINT64_C(1) << 32)
+#define AMDGPU_CCMD_BO_VA_OP_SPARSE_BO (1 << 0)
 
 /*
  * AMDGPU_CCMD_CS_SUBMIT