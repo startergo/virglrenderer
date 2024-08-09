Vera-Firefly/Virglrenderer-test

commit e4f463e5b8d79a13737e5551cbabbd1180eff077

Add d3d_tex2d to vrend_resource
Add a pointer to the D3D texture associated with the resource, if
available. Return it with virgl_renderer_resource_info_ext.

Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
Acked-by: Gert Wollny <gert.wollny@collabora.com>
Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/1103>

src/vrend_renderer.c
@@ -8375,7 +8375,10 @@ void vrend_renderer_resource_destroy(struct vrend_resource *res)
    if (res->gbm_bo)
       gbm_bo_destroy(res->gbm_bo);
 #endif
-
+#ifdef WIN32
+   if (res->d3d_tex2d)
+      res->d3d_tex2d->lpVtbl->Release(res->d3d_tex2d);
+#endif
    free(res);
 }
 
@@ -12281,6 +12284,24 @@ void vrend_renderer_resource_get_info(struct pipe_resource *pres,
    info->format = res->base.format;
    info->flags = res->y_0_top ? VIRGL_RESOURCE_Y_0_TOP : 0;
    info->stride = util_format_get_nblocksx(res->base.format, u_minify(res->base.width0, 0)) * elsize;
+}
+
+int
+vrend_renderer_resource_d3d11_texture2d(struct pipe_resource *pres, void **d3d_tex2d)
+{
+#ifdef WIN32
+   struct vrend_resource *res = (struct vrend_resource *)pres;
+
+   if (!res->d3d_tex2d)
+      return EINVAL;
+
+   *d3d_tex2d = res->d3d_tex2d;
+   return 0;
+#else
+   (void)pres;
+   (void)d3d_tex2d;
+   return ENOTSUP;
+#endif
 }
 
 void vrend_renderer_get_cap_set(uint32_t cap_set, uint32_t *max_ver,

src/virglrenderer.c
@@ -464,8 +464,11 @@ void virgl_renderer_ctx_detach_resource(int ctx_id, int res_handle)
 }
 
 static int virgl_renderer_resource_get_info_common(int res_handle,
-                                                   struct virgl_renderer_resource_info *info)
+                                                   struct virgl_renderer_resource_info *info,
+                                                   UNUSED void **d3d_tex2d)
 {
+   int ret = 0;
+
    TRACE_FUNC();
    struct virgl_resource *res = virgl_resource_lookup(res_handle);
 
@@ -478,7 +481,12 @@ static int virgl_renderer_resource_get_info_common(int res_handle,
                                     (struct vrend_renderer_resource_info *)info);
    info->handle = res_handle;
 
-   return 0;
+#ifdef WIN32
+   if (d3d_tex2d)
+      ret = vrend_renderer_resource_d3d11_texture2d(res->pipe_resource, d3d_tex2d);
+#endif
+
+   return ret;
 }
 
 int virgl_renderer_resource_get_info(int res_handle,
@@ -487,7 +495,7 @@ int virgl_renderer_resource_get_info(int res_handle,
    TRACE_FUNC();
    int ret;
 
-   if ((ret = virgl_renderer_resource_get_info_common(res_handle, info)) != 0)
+   if ((ret = virgl_renderer_resource_get_info_common(res_handle, info, NULL)) != 0)
        return ret;
 
    if (state.winsys_initialized) {
@@ -508,7 +516,9 @@ int virgl_renderer_resource_get_info_ext(int res_handle,
    TRACE_FUNC();
    int ret;
 
-   if ((ret = virgl_renderer_resource_get_info_common(res_handle, &info_ext->base)) != 0)
+   if ((ret = virgl_renderer_resource_get_info_common(res_handle,
+                                                      &info_ext->base,
+                                                      &info_ext->d3d_tex2d)) != 0)
       return ret;
 
    info_ext->version = VIRGL_RENDERER_RESOURCE_INFO_EXT_VERSION;

src/virglrenderer.h
@@ -321,6 +321,7 @@ struct virgl_renderer_resource_info_ext {
    bool has_dmabuf_export;
    int planes;
    uint64_t modifiers;
+   void *d3d_tex2d;
 };
 
 VIRGL_EXPORT int virgl_renderer_resource_get_info(int res_handle,

src/vrend_renderer.h
@@ -39,6 +39,9 @@
 #endif
 #include "virgl_hw.h"
 #include <epoxy/gl.h>
+#ifdef WIN32
+#include <d3d11.h>
+#endif
 
 typedef void *virgl_gl_context;
 typedef void *virgl_gl_drawable;
@@ -92,6 +95,9 @@ struct vrend_resource {
    uint64_t mipmap_offsets[VR_MAX_TEXTURE_2D_LEVELS];
    void *gbm_bo, *egl_image;
    void *aux_plane_egl_image[VIRGL_GBM_MAX_PLANES];
+#ifdef WIN32
+   ID3D11Texture2D *d3d_tex2d;
+#endif
 
    uint64_t size;
    GLbitfield buffer_storage_flags;
@@ -566,5 +572,9 @@ void vrend_renderer_get_meminfo(struct vrend_context *ctx, uint32_t res_handle);
 void vrend_context_emit_string_marker(struct vrend_context *ctx, GLsizei length, const char * message);
 
 struct vrend_video_context *vrend_context_get_video_ctx(struct vrend_context *ctx);
+
+int
+vrend_renderer_resource_d3d11_texture2d(struct pipe_resource *res, void **handle);
+
 
 #endif

startergo/virglrenderer

commit 6810fe91a3db6831c4c41102fa058e850be5dbee

vrend: Fix resource_get_info_ext() failing for d3d resources
The virgl_renderer_resource_get_info_ext() errors out when built on Windows
and if D3D feature isn't enabled at runtime. This leads to virgl failing for
Qemu on Windows when Qemu not using Angle, and thus, not enabling the D3D.

Fixes: e4f463e5b8d7 ("Add d3d_tex2d to vrend_resource")
Reported-on: https://gitlab.com/qemu-project/qemu/-/issues/2490
Signed-off-by: Dmitry Osipenko <dmitry.osipenko@collabora.com>
Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/1418>

src/vrend_renderer.c
@@ -13081,6 +13081,9 @@ vrend_renderer_resource_d3d11_texture2d(struct pipe_resource *pres, void **d3d_t
 #ifdef WIN32
    struct vrend_resource *res = (struct vrend_resource *)pres;
 
+   if (!vrend_state.d3d_share_texture)
+      return 0;
+
    if (!res->d3d_tex2d)
       return EINVAL;
 