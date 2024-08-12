startergo/virglrenderer

commit 6f39c1e0794b94228f397be4460bf65722205b8b

drm: drm_context_init() can fail
If we are out of memory, drm_context_init() can fail.  Clean up and
return an error in that case.

Signed-off-by: Demi Marie Obenour <demi@invisiblethingslab.com>
Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/1417>

src/drm/drm_context.c
@@ -223,15 +223,19 @@ drm_context_detach_resource(struct virgl_context *vctx, struct virgl_resource *r
    drm_context_free_object(dctx, obj);
 }
 
-void
+bool
 drm_context_init(struct drm_context *dctx, int fd,
                  const struct drm_ccmd *ccmd_dispatch, unsigned int dispatch_size)
 {
    /* Indexed by res_id: */
    dctx->resource_table = _mesa_hash_table_create_u32_keys(NULL);
+   if (dctx->resource_table == NULL)
+      return false;
 
    /* Indexed by blob_id, but only lower 32b of blob_id are used: */
    dctx->blob_table = _mesa_hash_table_create_u32_keys(NULL);
+   if (dctx->blob_table)
+      goto fail_blob_table;
 
    dctx->ccmd_dispatch = ccmd_dispatch;
    dctx->dispatch_size = dispatch_size;
@@ -246,6 +250,11 @@ drm_context_init(struct drm_context *dctx, int fd,
    dctx->base.get_fencing_fd = drm_context_get_fencing_fd;
    dctx->base.retire_fences = drm_context_retire_fences;
    dctx->base.detach_resource = drm_context_detach_resource;
+   return true;
+fail_blob_table:
+   _mesa_hash_table_destroy(dctx->resource_table, NULL);
+   dctx->resource_table = NULL;
+   return false;
 }
 
 static void

src/drm/amdgpu/amdgpu_renderer.c
@@ -1175,6 +1175,8 @@ amdgpu_renderer_create(int fd, size_t debug_len, const char *debug_name)
    if (ctx == NULL)
       goto fail;
    ctx->debug_name = strndup(debug_name, debug_len);
+   if (ctx->debug_name == NULL)
+      goto fail;
    ctx->debug = -1;
    ctx->dev = dev;
    const char *d = getenv("DEBUG");
@@ -1184,7 +1186,8 @@ amdgpu_renderer_create(int fd, size_t debug_len, const char *debug_name)
    print(1, "amdgpu_renderer_create name=%s fd=%d (from %d) -> dev=%p", ctx->debug_name, fd,
          amdgpu_device_get_fd(ctx->dev), (void*)ctx->dev);
 
-   drm_context_init(&ctx->base, -1, ccmd_dispatch, ARRAY_SIZE(ccmd_dispatch));
+   if (!drm_context_init(&ctx->base, -1, ccmd_dispatch, ARRAY_SIZE(ccmd_dispatch)))
+      goto fail_init;
 
    ctx->base.base.destroy = amdgpu_renderer_destroy;
    ctx->base.base.attach_resource = amdgpu_renderer_attach_resource;
@@ -1195,6 +1198,8 @@ amdgpu_renderer_create(int fd, size_t debug_len, const char *debug_name)
    ctx->base.free_object = amdgpu_renderer_free_object;
 
    ctx->id_to_ctx = _mesa_hash_table_u64_create(NULL);
+   if (ctx->id_to_ctx == NULL)
+      goto fail_hash_table;
 
    /* Ring 0 is for CPU execution. */
    /* TODO: add a setting to control which queues are exposed to the
@@ -1229,10 +1234,13 @@ amdgpu_renderer_create(int fd, size_t debug_len, const char *debug_name)
 
 fail_context_deinit:
    _mesa_hash_table_u64_destroy(ctx->id_to_ctx, NULL);
+fail_hash_table:
    drm_context_deinit(&ctx->base);
+fail_init:
+   free((void*)ctx->debug_name);
 fail:
-   amdgpu_device_deinitialize(dev);
    free(ctx);
+   amdgpu_device_deinitialize(dev);
    close(fd);
    return NULL;
 }

src/drm/drm_context.h
@@ -61,7 +61,7 @@ struct drm_context {
 };
 DEFINE_CAST(virgl_context, drm_context)
 
-void drm_context_init(struct drm_context *dctx, int fd,
+bool drm_context_init(struct drm_context *dctx, int fd,
                       const struct drm_ccmd *ccmd_dispatch, unsigned int dispatch_size);
 
 void drm_context_deinit(struct drm_context *dctx);

src/drm/msm/msm_renderer.c
@@ -1010,7 +1010,10 @@ msm_renderer_create(int fd, UNUSED size_t debug_len, UNUSED const char *debug_na
    if (!mctx)
       return NULL;
 
-   drm_context_init(&mctx->base, fd, ccmd_dispatch, ARRAY_SIZE(ccmd_dispatch));
+   if (!drm_context_init(&mctx->base, fd, ccmd_dispatch, ARRAY_SIZE(ccmd_dispatch))) {
+      free(mctx);
+      return NULL;
+   }
 
    /* Indexed by submitqueue-id: */
    mctx->sq_to_ring_idx_table = _mesa_hash_table_create_u32_keys(NULL);

commit 4fc19d919fafba78d8578708da5c1e9fecfabf40

drm: context: Fix failing drm_context_init()
Fix typo in the drm_context_init() that makes it to always fail.

Fixes: 6f39c1e0794b ("drm: drm_context_init() can fail")
Signed-off-by: Dmitry Osipenko <dmitry.osipenko@collabora.com>
Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/1420>

src/drm/drm_context.c
@@ -234,7 +234,7 @@ drm_context_init(struct drm_context *dctx, int fd,
 
    /* Indexed by blob_id, but only lower 32b of blob_id are used: */
    dctx->blob_table = _mesa_hash_table_create_u32_keys(NULL);
-   if (dctx->blob_table)
+   if (dctx->blob_table == NULL)
       goto fail_blob_table;
 
    dctx->ccmd_dispatch = ccmd_dispatch;