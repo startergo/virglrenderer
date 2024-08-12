startergo/virglrenderer

commit 8fad50f83f2b34dfa38d1019f517df56c694c014

drm/amdgpu: remove amdgpu_ccmd_cs_submit_req::bo_number
This information is already encoded in the AMDGPU_CHUNK_ID_BO_HANDLES,
so don't require the guest to set it twice.

Signed-off-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/1415>

src/drm/amdgpu/amdgpu_renderer.c
@@ -758,7 +758,7 @@ amdgpu_ccmd_cs_submit(struct drm_context *dctx, struct vdrm_ccmd_req *hdr)
 {
    const struct amdgpu_ccmd_cs_submit_req *req = to_amdgpu_ccmd_cs_submit_req(hdr);
    struct amdgpu_context *ctx = to_amdgpu_context(dctx);
-   struct drm_amdgpu_bo_list_in bo_list_in;
+   struct drm_amdgpu_bo_list_in bo_list_in = { 0 };
    struct drm_amdgpu_cs_chunk_fence user_fence;
    struct drm_amdgpu_cs_chunk_sem syncobj_in = { 0 };
    const struct drm_amdgpu_bo_list_entry *bo_handles_in = NULL;
@@ -834,11 +834,7 @@ amdgpu_ccmd_cs_submit(struct drm_context *dctx, struct vdrm_ccmd_req *hdr)
       const void *input = (const char *)req + offset;
 
       if (chunk_id == AMDGPU_CHUNK_ID_BO_HANDLES) {
-         uint32_t bo_count = req->bo_number;
-         if (!validate_chunk_inputs(bo_count, typeof(*bo_handles_in))) {
-            r = -EINVAL;
-            goto end;
-         }
+         uint32_t bo_count = len / sizeof(*bo_handles_in);
 
          if (bo_list != NULL) {
             print(0, "Refusing to allocate multiple BO lists");
@@ -969,13 +965,13 @@ amdgpu_ccmd_cs_submit(struct drm_context *dctx, struct vdrm_ccmd_req *hdr)
    }
 
    if (r != 0 || ctx->debug >= 4) {
-      print(1, "GPU submit used %d BOs:", req->bo_number);
+      print(1, "GPU submit used %d BOs:", bo_list_in.bo_number);
       print(1, "Used | Resource ID ");
       print(1, "-----|-------------");
       hash_table_foreach (ctx->base.resource_table, entry) {
          const struct amdgpu_object *o = entry->data;
          bool used = false;
-         for (unsigned j = 0; j < req->bo_number && !used; j++) {
+         for (unsigned j = 0; j < bo_list_in.bo_number && !used; j++) {
             if (bo_handles_in[j].bo_handle == o->base.res_id)
                used = true;
          }

src/drm/amdgpu/amdgpu_virtio_proto.h
@@ -123,7 +123,7 @@ struct amdgpu_ccmd_cs_submit_req {
 
    uint32_t ctx_id;
    uint32_t num_chunks; /* limited to AMDGPU_CCMD_CS_SUBMIT_MAX_NUM_CHUNKS */
-   uint32_t bo_number;
+   uint32_t pad;
    uint32_t ring_idx;
 
    /* Starts with a descriptor array:

commit 7c58a219e0814bbd2114e1cc21513e24a1327205

drm/amdgpu: Validate bo_count alignment
Even though bo_count is derived from the length, the offset must still
be checked for alignment.

Signed-off-by: Demi Marie Obenour <demi@invisiblethingslab.com>
Fixes: 8fad50f83f2b (drm/amdgpu: remove amdgpu_ccmd_cs_submit_req::bo_number)
Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/1419>

src/drm/amdgpu/amdgpu_renderer.c
@@ -840,6 +840,8 @@ amdgpu_ccmd_cs_submit(struct drm_context *dctx, struct vdrm_ccmd_req *hdr)
          r = -EINVAL;
          goto end;
       }
+      /* This macro must be used to validate the offset AND count.  Even if
+       * the count is trusted, the offset must still be validated! */
 #define validate_chunk_inputs(count, type) \
    validate_chunk_inputs(offset, len, ctx, count, sizeof(type), alignof(type))
 
@@ -847,6 +849,10 @@ amdgpu_ccmd_cs_submit(struct drm_context *dctx, struct vdrm_ccmd_req *hdr)
 
       if (chunk_id == AMDGPU_CHUNK_ID_BO_HANDLES) {
          uint32_t bo_count = len / sizeof(*bo_handles_in);
+         if (!validate_chunk_inputs(bo_count, typeof(*bo_handles_in))) {
+            r = -EINVAL;
+            goto end;
+         }
 
          if (bo_list != NULL) {
             print(0, "Refusing to allocate multiple BO lists");