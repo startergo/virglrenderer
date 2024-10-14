CodeLinaro-mirror/vcl_virglrenderer

vcomp: Renderer
Initialize a vcomp renderer during virglrenderer initialization.

Signed-off-by: Antonio Caggiano <quic_acaggian@quicinc.com>

src/vcl/vcomp_renderer.h
@@ -10,10 +10,33 @@
 
 #ifdef ENABLE_VCL
 
+int vcomp_renderer_init(void);
+
+void vcomp_renderer_fini(void);
+
+void vcomp_renderer_reset(void);
+
 size_t
 vcomp_get_capset(void *capset);
 
 #else /* ENABLE_VCL */
+
+static inline int
+vcomp_renderer_init(void)
+{
+   virgl_log("OpenCL support was not enabled in virglrenderer\n");
+   return -1;
+}
+
+static inline void
+vcomp_renderer_fini(void)
+{
+}
+
+static inline void
+vcomp_renderer_reset(void)
+{
+}
 
 static size_t
 vcomp_get_capset(UNUSED void *capset)

startergo/virglrenderer

src/vcl/vcomp_renderer.h
@@ -10,10 +10,33 @@
 
 #ifdef ENABLE_VCL
 
+int vcomp_renderer_init(void);
+
+void vcomp_renderer_fini(void);
+
+void vcomp_renderer_reset(void);
+
 size_t
 vcomp_get_capset(void *capset);
 
 #else /* ENABLE_VCL */
+
+static inline int
+vcomp_renderer_init(void)
+{
+   virgl_error("OpenCL support was not enabled in virglrenderer\n");
+   return -1;
+}
+
+static inline void
+vcomp_renderer_fini(void)
+{
+}
+
+static inline void
+vcomp_renderer_reset(void)
+{
+}
 
 static size_t
 vcomp_get_capset(UNUSED void *capset)