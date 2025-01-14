Vera-Firefly/Virglrenderer-test

commit cc6c432eda5dea8e15a8646b3310b4c78cfd9fc7

drm/msm: Support inter-context fence sharing
Support sharing in/out fences between contexts by enabling new fence
passing feature. Now MSM contexts can offload fence waits from guest
to host.

Signed-off-by: Dmitry Osipenko <dmitry.osipenko@collabora.com>
Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/1138>

src/drm/msm/msm_renderer.c
@@ -19,6 +19,7 @@
 
 #include "util/anon_file.h"
 #include "util/hash_table.h"
+#include "util/libsync.h"
 #include "util/macros.h"
 #include "util/os_file.h"
 #include "util/u_atomic.h"
@@ -917,15 +918,24 @@ msm_ccmd_gem_submit(struct msm_context *mctx, const struct msm_ccmd_req *hdr)
    for (uint32_t i = 0; i < req->nr_bos; i++)
       bos[i].handle = handle_from_res_id(mctx, bos[i].handle);
 
+   uint32_t fence_flags = MSM_SUBMIT_FENCE_FD_OUT | MSM_SUBMIT_FENCE_SN_IN;
+
+   int in_fence_fd = virgl_context_take_in_fence_fd(&mctx->base);
+   if (in_fence_fd >= 0)
+         fence_flags |= MSM_SUBMIT_FENCE_FD_IN;
+
    struct drm_msm_gem_submit args = {
-      .flags = req->flags | MSM_SUBMIT_FENCE_FD_OUT | MSM_SUBMIT_FENCE_SN_IN,
+      .flags = req->flags | fence_flags,
       .fence = req->fence,
+      .fence_fd = in_fence_fd,
       .nr_bos = req->nr_bos,
       .nr_cmds = req->nr_cmds,
       .bos = VOID2U64(bos),
       .cmds = VOID2U64(&req->payload[req->nr_bos * sizeof(struct drm_msm_gem_submit_bo)]),
       .queueid = req->queue_id,
    };
+
+   close(in_fence_fd);
 
    int ret = drmCommandWriteRead(mctx->fd, DRM_MSM_GEM_SUBMIT, &args, sizeof(args));
    drm_dbg("fence=%u, ret=%d", args.fence, ret);
@@ -1322,6 +1332,7 @@ msm_renderer_create(int fd)
    mctx->base.get_fencing_fd = msm_renderer_get_fencing_fd;
    mctx->base.retire_fences = msm_renderer_retire_fences;
    mctx->base.submit_fence = msm_renderer_submit_fence;
+   mctx->base.supports_fence_sharing = true;
 
    return &mctx->base;
 }

startergo/virglrenderer

commit 1a30500183d8b1292ed7e8a567194974b69c7890

drm: msm: Fix fence-sharing
We want to close the fence fd _after_ the submit ioctl.

Fixes: cc6c432eda5d drm/msm: ("Support inter-context fence sharing")
Signed-off-by: Rob Clark <robdclark@chromium.org>
Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/1462>

src/drm/msm/msm_renderer.c
@@ -761,8 +761,6 @@ msm_ccmd_gem_submit(struct drm_context *dctx, struct vdrm_ccmd_req *hdr)
       .queueid = req->queue_id,
    };
 
-   close(in_fence_fd);
-
    int ret = drmCommandWriteRead(dctx->fd, DRM_MSM_GEM_SUBMIT, &args, sizeof(args));
    drm_dbg("fence=%u, ret=%d", args.fence, ret);
 
@@ -786,6 +784,8 @@ msm_ccmd_gem_submit(struct drm_context *dctx, struct vdrm_ccmd_req *hdr)
    }
 
 out:
+   if (in_fence_fd >= 0)
+      close(in_fence_fd);
    if (!bos_on_stack)
       free(bos);
    return 0;