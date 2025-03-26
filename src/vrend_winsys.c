startergo/virglrenderer

commit 82e6987cc578019652dedd8e47839fbe352f16ab

vrend: fix modifier query overflow
eglExportDMABUFImageQueryMESA will return array of modifiers,
one for each plane. So we should pass an array as argument to
prevent overflow when multi-plane case.

See also: https://github.com/KhronosGroup/EGL-Registry/pull/211

Signed-off-by: Qiang Yu <yuq825@gmail.com>
Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/1493>

src/vrend_winsys.c
@@ -208,13 +208,13 @@ int vrend_winsys_has_gl_colorspace(void)
 
 int vrend_winsys_get_attrs_for_texture(uint32_t tex_id, uint32_t format, int *fourcc,
                                        bool *has_dmabuf_export,
-                                       int *planes, uint64_t *modifiers)
+                                       int *planes, uint64_t *modifier)
 {
 #ifdef ENABLE_GBM
    if (use_context == CONTEXT_EGL)
       return virgl_egl_get_attrs_for_texture(egl, tex_id, format, fourcc,
                                              has_dmabuf_export,
-                                             planes, modifiers);
+                                             planes, modifier);
 #else
    (void)tex_id;
    (void)format;

src/vrend_winsys.h
@@ -60,7 +60,7 @@ int vrend_winsys_has_gl_colorspace(void);
 
 int vrend_winsys_get_attrs_for_texture(uint32_t tex_id, uint32_t format, int *fourcc,
                                        bool *has_dmabuf_export,
-                                       int *planes, uint64_t *modifiers);
+                                       int *planes, uint64_t *modifier);
 int vrend_winsys_get_fd_for_texture(uint32_t tex_id, int *fd);
 int vrend_winsys_get_fd_for_texture2(uint32_t tex_id, int *fd, int *stride, int *offset);
 

src/vrend_winsys_egl.c
@@ -663,13 +663,14 @@ virgl_renderer_gl_context virgl_egl_get_current_context(UNUSED struct virgl_egl 
 int virgl_egl_get_attrs_for_texture(struct virgl_egl *egl, uint32_t tex_id,
                                     uint32_t format, int *fourcc,
                                     bool *has_dmabuf_export,
-                                    int *planes, uint64_t *modifiers)
+                                    int *planes, uint64_t *modifier)
 {
    int ret = EINVAL;
    uint32_t gbm_format = 0;
 
    EGLImageKHR image;
    EGLBoolean success;
+   uint64_t modifiers[4];
 
    if (!has_bit(egl->extension_bits, EGL_MESA_IMAGE_DMA_BUF_EXPORT)) {
       ret = 0;
@@ -686,6 +687,9 @@ int virgl_egl_get_attrs_for_texture(struct virgl_egl *egl, uint32_t tex_id,
                                            modifiers);
    if (!success)
       goto out_destroy;
+   /* Only first modifier matters. */
+   if (modifier)
+      *modifier = modifiers[0];
    ret = 0;
  out_destroy:
    eglDestroyImageKHR(egl->egl_display, image);

src/vrend_winsys_egl.h
@@ -64,7 +64,7 @@ bool virgl_has_egl_khr_gl_colorspace(struct virgl_egl *egl);
 #ifdef ENABLE_GBM
 int virgl_egl_get_attrs_for_texture(struct virgl_egl *egl, uint32_t tex_id, uint32_t format,
                                     int *fourcc, bool *has_dmabuf_export,
-                                    int *planes, uint64_t *modifiers);
+                                    int *planes, uint64_t *modifier);
 
 int virgl_egl_get_fd_for_texture(struct virgl_egl *egl, uint32_t tex_id, int *fd);
 

commit b449fdd33884d1c406a570608648fb6739064f15

vrend: fix a compile failure
Can occur with just -Dplatforms= -Dvenus=true

Fixes: 82e6987c ("vrend: fix modifier query overflow")
Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/1497>

src/vrend_winsys.c
@@ -220,7 +220,7 @@ int vrend_winsys_get_attrs_for_texture(uint32_t tex_id, uint32_t format, int *fo
    (void)format;
    (void)fourcc;
    (void)planes;
-   (void)modifiers;
+   (void)modifier;
    if (has_dmabuf_export)
       *has_dmabuf_export = false;
 #endif