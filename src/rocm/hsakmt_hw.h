008cef8212b396278e69084e758dfd6e4b25ee74
Honglei Huang <Honglei1.Huang@amd.com>
2025-12-19 10:57:00 +0800

    rocm: add protocol definitions and utilities
    Add core protocol definitions for rocm native contect:
    - hsakmt_hw.h: Hardware definitions
    - hsakmt_virtio_proto.h: Virtio protocol structures (501 lines)
    - hsakmt_util.h: Utility macros and helpers
    
    These files define the communication protocol between guest
    and host for ROCm native context virtualization.
    
    Change-Id: I49927f90945c884bcdb2019e9054debbdcb8c015
    Signed-off-by: Honglei Huang <Honglei1.Huang@amd.com>

---
+++ src/rocm/hsakmt_hw.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2025 Advanced Micro Devices, Inc
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef HSAKMT_HW_H_
+#define HSAKMT_HW_H_
+
+#include "drm_hw.h"
+
+#define vhsakmt_shmem_base vdrm_shmem
+#define vhsakmt_ccmd_req vdrm_ccmd_req
+#define vhsakmt_ccmd_rsp vdrm_ccmd_rsp
+
+#endif /* HSAKMT_HW_H_ */
